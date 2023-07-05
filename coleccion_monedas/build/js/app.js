1e32135e570f58ace5af91700bfd166d8d92c218
pablarce <pablo.arce1@alumnos.uneatlantico.es>
2023-07-05 15:26:59 +0200

    New images + overlay

--- coleccion_monedas/build/js/app.js
+++ coleccion_monedas/build/js/app.js
@@ -15,9 +15,34 @@ function crearGaleria() {
             <source srcset="build/assets/img/thumb/imagen${i}.webp" type="image/webp">
             <img loading="lazy" width="200" height="300" src="build/assets/img/thumb/imagen${i}.jpg" alt="Imagen galeria">
         `;
-        console.log(i);
+        imagen.onclick = function () {
+            mostrarImagen(i);
+        }
         galeria.appendChild(imagen);
     }
+}
+
+
+function mostrarImagen(i) {
+    const imagen = document.createElement("picture");
+
+    imagen.innerHTML = `
+        <source srcset="build/assets/img/complete/imagen${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/assets/img/complete/imagen${i}.jpg" alt="Imagen galeria">
+    `;
+
+    const overlay = document.createElement("div");
+    overlay.appendChild(imagen);
+    overlay.classList.add("overlay");
+
+    const cerrarModal = document.createElement("p");
+    cerrarModal.textContent = "X";
+    cerrarModal.classList.add("btn-cerrar");
+
+    overlay.appendChild(cerrarModal);
+
+    const body = document.querySelector("body");
+    body.appendChild(overlay);
 }
 
 iniciarApp();
