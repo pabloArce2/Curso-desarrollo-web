32a62302535a85459bfc2b45e0bc4a29c9f27714
pablarce <pablo.arce1@alumnos.uneatlantico.es>
2023-07-11 16:29:29 +0200

    Algunos comandos que realice en las pruebas:

--- test_mysql/test1.sql
+++ test_mysql/test1.sql
@@ -1,6 +1,6 @@
 show databases;
 use holamundo;
-CREATE TABLE reservations (
+CREATE TABLE services (
 	id int(12) not null auto_increment,
     name varchar(64) not null,
     price decimal(6,2) not null,
@@ -30,3 +30,46 @@ CREATE TABLE reservations (
 
 select count(id), date from reservations group by date order by count(id) desc;
 select sum(price) as totalService from servicios;
+select * from servicios where name like "Corte%";
+select * from servicios where name like "%Lavado%";
+select concat(name," ",surname) as full_name from reservations;
+select * from reservations where concat(name," ",surname) like "%Ana Preciado%";
+
+use ejercicio1;
+create table clientes (
+    id int(11) not null auto_increment,
+    name varchar(60) not null,
+    surname varchar(60) not null,
+    phone varchar(10) not null unique,
+    email varchar(30) not null unique,
+    primary key (id)
+);
+create table citas (
+    id int(11) not null auto_increment,
+    date date not null,
+    time time not null,
+    clientId int(11) not null,
+    primary key (id),
+    key clientId (clientId),
+    constraint client_FK foreign key (clientId) references clientes (id)
+);
+select * from citas inner join clientes on clientes.id = citas.clientId;
+create table citasServicios (
+    id int(11) auto_increment,
+    citaId int(11) not null,
+    servicioId int(11) not null,
+    primary key (id),
+    key clientId (citaId),
+    constraint citas_FK foreign key (citaId) references citas (id),
+    key servicioId (servicioId),
+    constraint servicio_FK foreign key (servicioId) references services (id)
+);
+
+select * from citasServicios 
+left join citas on citas.id = citasServicios.citaId
+left join services on services.id = citasServicios.servicioId;
+
+select * from citasServicios
+left join citas on citas.id = citasServicios.citaId
+left join clientes on citas.clientId = clientes.id
+left join services on services.id = citasServicios.servicioId;
